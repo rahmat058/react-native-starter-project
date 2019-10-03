4ad7670bf7830d36d5287f03f649e57d32186e24
rahmat058 <md.kazi.rahmat@gmail.com>
2019-10-03 21:53:31 +0600

    constants file is added done for styles

---
+++ src/styles/constants.js
@@ -0,0 +1,42 @@
+const sizes = {
+  // global sizes
+  base: 16,
+  font: 14,
+  radius: 6,
+  padding: 25,
+
+  // font sizes
+  h1: 26,
+  h2: 20,
+  h3: 18,
+  title: 18,
+  header: 16,
+  body: 14,
+  caption: 12,
+};
+
+const fonts = {
+  h1: {
+    fontSize: sizes.h1
+  },
+  h2: {
+    fontSize: sizes.h2
+  },
+  h3: {
+    fontSize: sizes.h3
+  },
+  header: {
+    fontSize: sizes.header
+  },
+  title: {
+    fontSize: sizes.title
+  },
+  body: {
+    fontSize: sizes.body
+  },
+  caption: {
+    fontSize: sizes.caption
+  },
+};
+
+export { sizes, fonts };
