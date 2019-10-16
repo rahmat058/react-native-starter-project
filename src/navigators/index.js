1008c66670749b43f6557ed706544e1ae8f3e8e5
Kazi Rahamatullah <md.kazi.rahmat@gmail.com>
2019-10-16 16:15:36 +0600

    Animated Switch Navigator set up is done

--- src/navigators/index.js
+++ src/navigators/index.js
@@ -1,17 +1,33 @@
-import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+import React from 'react';
+import createAnimatedSwitchNavigator from 'react-navigation-animated-switch';
+import { Transition } from 'react-native-reanimated';
+import { createAppContainer } from 'react-navigation';
 import { AuthStackNavigator } from './PublicAuthNavigator';
 import { AppDrawerNavigator } from './PrivateAppNavigator';
 
 import AuthLoadingScreen from '../screens/AuthLoadingScreen';
 
-const AppSwitchNavigator = createSwitchNavigator({
-  AuthLoadingScreen,
-  Auth: AuthStackNavigator,
-  App: {
-    screen: AppDrawerNavigator,
+const AppSwitchNavigator = createAnimatedSwitchNavigator(
+  {
+    AuthLoadingScreen,
+    Auth: AuthStackNavigator,
+    App: {
+      screen: AppDrawerNavigator,
+    },
   },
-}, {
-  initialRouteName: 'AuthLoadingScreen',
-});
+  {
+    initialRouteName: 'AuthLoadingScreen',
+    transition: (
+      <Transition.Together>
+        <Transition.Out
+          type="slide-bottom"
+          durationMs={400}
+          interpolation="easeIn"
+        />
+        <Transition.In type="fade" durationMs={500} />
+      </Transition.Together>
+    ),
+  },
+);
 
 export const RootAppNavigator = createAppContainer(AppSwitchNavigator);
